e97375e81c7005a6ca56c8666ed42a688cd108a8
Jenny <jenny.makijaakkola@gmail.com>
2024-11-07 21:55:31 -0800

    lisää Header-komponentin

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from './components/Header';
 import Booster from './components/Booster';
 import Balance from './components/Balance';
 import Lemon from './components/Lemon';
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <Header>lemon clicker</Header>
         <Balance total="157" />
         <Lemon />
         <Booster value="3.2" />
