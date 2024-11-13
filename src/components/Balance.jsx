633300d7061d17f9594305f237d968d8478a2778
Jenny <jenny.makijaakkola@gmail.com>
2024-11-13 09:42:17 -0800

    lisää lukujen sievennyksen

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,6 +1,8 @@
+import shortenNumber from '../utils/shortenNumber';
+
 function Balance(props) {
   // Poimitaan komponentille välitetty total-arvo
-  const total = props.total;
+  const total = shortenNumber(props.total);
   return (
     <div className="balance">
       <div>lemons</div>
--- src/components/Booster.jsx
+++ src/components/Booster.jsx
@@ -1,6 +1,8 @@
+import shortenNumber from '../utils/shortenNumber';
+
 function Booster(props) {
   // Poimitaan komponentille välitetty value-arvo
-  const value = props.value;
+  const value = shortenNumber(props.value);
   
   return (
     <div className="booster">
