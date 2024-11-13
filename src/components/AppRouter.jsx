3cdd66f2ae5bb12917b0643f6752fe3f700f5cad
Jenny <jenny.makijaakkola@gmail.com>
2024-11-12 18:16:38 -0800

    lisää tuotelistataulukon

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,6 +4,8 @@ import ErrorPage from '../pages/ErrorPage';
 import Clicker from '../pages/Clicker';
 import Store from '../pages/Store';
 import Settings from '../pages/Settings';
+import Credits from '../pages/Credits';
+
 function AppRouter(props) {
  
   const router = createBrowserRouter([
@@ -15,6 +17,7 @@ function AppRouter(props) {
         { path: "", element: <Clicker stats={props.stats} handleClick={props.handleClick} /> },
         { path: "store", element: <Store stats={props.stats} />},
         { path: "settings", element: <Settings stats={props.stats} />}, 
+        { path: "credits", element: <Credits /> }
       ]
     }
   ]);
---
+++ src/pages/Credits.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Credits = () => {
+  return (
+    <div>
+      <h1>Credits</h1>
+      <p>Mallikokeilu by Miss_Revenant</p>
+      <p>Peli-iloja!</p>
+    </div>
+  );
+};
+
+export default Credits;
